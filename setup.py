ce313dd8b4d593f8480b650b183eb608af4fdacb
KieranLitschel <kieran.litschel@hotmail.co.uk>
2018-09-06 13:02:59 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Built for the implementation of Keras in Tensorflow. Behaves similarly to GridSearchCV and "
                 "RandomizedSearchCV in Sci-Kit learn, but allows for progress to be saved between folds and "
                 "for fitting and scoring folds in parallel.",
-    py_modules=['KerasSearchCV', 'KerasSearchCVWorker'],
+    py_modules=['KerasSearchCV', 'KerasSearchCVWorker', 'SKLearnSearchCVWorker'],
     install_requires=['absl-py==0.4.0',
                       'astor==0.7.1',
                       'gast==0.2.0',
@@ -21,5 +21,6 @@ setup(
                       'termcolor==1.1.0',
                       'Werkzeug==0.14.1',
                       'dill==0.2.8.2',
-                      "h5py==2.8.0"]
+                      "h5py==2.8.0",
+                      "scipy=1.1.0"]
 )
